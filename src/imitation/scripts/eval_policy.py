f2a18d87c006546a602ba46f5f696cd0fa3f1c4a
Pavel <pavelc.git@posteo.net>
2022-05-03 11:30:08 -0700

    Pass gym3 env to gym3 recorder wrapper

--- src/imitation/scripts/eval_policy.py
+++ src/imitation/scripts/eval_policy.py
@@ -51,7 +51,9 @@ def video_wrapper_factory(log_dir: str, **kwargs):
         if not isinstance(env.unwrapped, ToGymEnv):
             return video_wrapper.VideoWrapper(env, directory=directory, **kwargs)
         else:
-            return VideoRecorderWrapper(env.unwrapped, directory=directory, **kwargs)
+            # This will be the gym3.Env, which VideoRecorderWrapper expects.
+            unwrapped_env = env.unwrapped.env
+            return VideoRecorderWrapper(unwrapped_env, directory=directory, **kwargs)
     return f
 
 
